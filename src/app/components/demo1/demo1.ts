35168ccb196f0fa355622dbc78aa1037fab49c73
David Weber <david.weber@w3tec.ch>
2016-03-04 23:08:40 +0100

    add component router middleware

--- src/app/components/demo1/demo1.ts
+++ src/app/components/demo1/demo1.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 import {Http} from 'angular2/http';
-import {Router} from 'angular2/router';
+import {Router, CanDeactivate, ComponentInstruction} from 'angular2/router';
+
+import Logger from './../../utils/logger.service';
 
 @Component({
   selector: 'demo1',
@@ -11,7 +13,11 @@ import {Router} from 'angular2/router';
   pipes: []
 })
 
-export class Demo1 {
+export class Demo1 implements CanDeactivate {
+
+  private _log: Logger = new Logger('Demo1');
+
+  public confirmText: string;
 
   constructor(http: Http, private _router: Router) {
     ;
@@ -22,5 +28,15 @@ export class Demo1 {
    */
   public goToHome(): void {
     this._router.parent.navigate(['Home']);
+  }
+
+  /**
+   * routerCanDeactivate
+   */
+  public routerCanDeactivate(next: ComponentInstruction, prev: ComponentInstruction) : any {
+    // Allow synchronous navigation (`true`) if no crisis or the crisis is unchanged.
+    let log = this._log.debug('routerCanDeactivate');
+    log(this.confirmText);
+    return this.confirmText === 'bubu';
   }
 }
