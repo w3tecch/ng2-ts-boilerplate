837b15c23b9910a38ca7f116938792460ba76ecf
David Weber <david.weber@w3tec.ch>
2016-03-04 22:53:45 +0100

    refactor some routing things

--- src/app/components/demo1/demo1.ts
+++ src/app/components/demo1/demo1.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {Http} from 'angular2/http';
+import {Router} from 'angular2/router';
 
 @Component({
   selector: 'demo1',
@@ -12,7 +13,14 @@ import {Http} from 'angular2/http';
 
 export class Demo1 {
 
-  constructor(http: Http) {
+  constructor(http: Http, private _router: Router) {
     ;
+  }
+
+  /**
+   * goToHome
+   */
+  public goToHome(): void {
+    this._router.parent.navigate(['Home']);
   }
 }
